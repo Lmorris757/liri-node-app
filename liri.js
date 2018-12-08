3ca5a69b5fbb435297a3e57dbd68354bb5d46810
LxMO <lmorris757@outlook.com>
2018-12-07 17:01:00 -0800

    version of code with functioning spotify, omdb, and bandsintown commands

--- liri.js
+++ liri.js
@@ -3,9 +3,55 @@
 require("dotenv").config();
 var axios = require("axios")
 var fs = require("fs");
+var moment = require("moment");
+moment().format("MM/DD/YYY");
 var keys = require("./keys.js");
 // "var Spotify" is reading keys.js //
 var Spotify = require("node-spotify-api");
 // "var inquire" is initializing the pkg needed to grab user input //
 var inquirer = require("inquirer");
 var spotify = new Spotify(keys.spotify);
+
+var search = process.argv[2]
+// console.log(search);
+
+var term = process.argv.slice(3).join(" ");
+// console.log(term);
+// The search parameters for Spotify //
+if (search === "spotify") {
+  console.log("searching for a song...");
+  spotify.search({ type: 'track', query: term }, function(err, response) {
+    if (err) {
+      return console.log('Error occurred: ' + err);
+    }
+  console.log("Artist: ", response.tracks.items[0].artists[0].name);
+  console.log("Song Title: ", response.tracks.items[0].name);
+  console.log("Preview Link: ", response.tracks.items[0].preview_url);
+  console.log("Album Name: ", response.tracks.items[0].album.name);
+  // console.log(response.tracks.items[0].name); 
+  });
+
+// Search parameters for OMDB **the "if" portion of the else statement narrows the type of search commands to only spotify or movie** //
+} else if (search === "movie") {
+  console.log("searching for a movie...");
+  var URL = "http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy";
+// using axios to grab the URL variable //
+  axios.get(URL).then(function (response) {
+    console.log("here's your movie info: ", response.data);
+  });
+} else if (search === "concert") {
+  var bitURL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
+
+  axios.get(bitURL).then(function (response) {
+    console.log(response.data[0].venue.name);
+    console.log(response.data[0].venue.country);
+  // The date and time for the concert command are working, but implementing momentjs is proving to be a struggle //
+    console.log(response.data[0].datetime);
+  })
+}
+else {
+  console.log("so...you're using this wrong.");
+}
+
+
+
