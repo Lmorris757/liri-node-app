e89516c4842de297332e9f5cd9be989aac8236fd
LxMO <lmorris757@outlook.com>
2018-12-07 21:57:59 -0800

    final

--- liri.js
+++ liri.js
@@ -37,21 +37,23 @@ if (search === "spotify") {
   var URL = "http://www.omdbapi.com/?t=" + term + "&y=&plot=short&apikey=trilogy";
 // using axios to grab the URL variable //
   axios.get(URL).then(function (response) {
-    console.log("here's your movie info: ", response.data);
+    console.log("here's your movie info: ", response.data.Title);
+    console.log("Released: ", response.data.Year);
+    console.log("IMDB Rating: ", response.data.imdbRating);
+    console.log("Rotten Tomatoes Rating: ", response.data.Ratings[1].Value);
+    console.log("Country: ", response.data.Country);
+    console.log("anguage: ", response.data.Language);
+    console.log("Plot: ", response.data.Plot);
+    console.log("Actors: ", response.data.Actors);
   });
 } else if (search === "concert") {
   var bitURL = "https://rest.bandsintown.com/artists/" + term + "/events?app_id=codingbootcamp";
 
-  axios.get(bitURL).then(function (response) {
-    console.log(response.data[0].venue.name);
-    console.log(response.data[0].venue.country);
-  // The date and time for the concert command are working, but implementing momentjs is proving to be a struggle //
-    console.log(response.data[0].datetime);
-  })
-}
-else {
-  console.log("so...you're using this wrong.");
-}
-
-
+    axios.get(bitURL).then(function (response) {
+      console.log(response.data[0].venue.name);
+      console.log(response.data[0].venue.country);
+    // The date and time for the concert command are working, but implementing momentjs is proving to be a struggle //
+      console.log(response.data[0].datetime);
+    })
+  }
 
